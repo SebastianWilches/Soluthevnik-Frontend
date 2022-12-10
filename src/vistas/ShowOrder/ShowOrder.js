cc1094ac45c7c5f15cfe18505ceac22752c8e0f9
Daniel Carreño <danielcarreno0102@gmail.com>
2022-12-10 18:09:08 -0500

    Front Terminado

--- src/vistas/ShowOrder/ShowOrder.js
+++ src/vistas/ShowOrder/ShowOrder.js
@@ -24,7 +24,7 @@ export default function ShowOrder() {
   };
 
   const handleVolver = () =>{
-    navigate('/createOrder', {state: {id: idAdmin}});
+    navigate('/createOrder', {state: {id: idAdmin.id}});
   }
 
   return (
