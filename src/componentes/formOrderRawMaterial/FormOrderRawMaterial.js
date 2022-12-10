8cac083e8d7050567e6781eaa790f92c964e8c01
unknown <sebastianwilches2@gmail.com>
2022-12-10 00:23:18 -0500

    OrderRawMaterial registra correctamente los pedidos con sus cantidades

--- src/componentes/formOrderRawMaterial/FormOrderRawMaterial.js
+++ src/componentes/formOrderRawMaterial/FormOrderRawMaterial.js
@@ -5,33 +5,82 @@ import Combobox from "react-widgets/Combobox";
 
 export default function FormOrderRawMaterial({ itemMaterial, supplier }) {
 
+
+    let [formMaterial, setFormMaterial] = useState([]);
+    const [ID_MaterialOrder, setID_MaterialOrder] = useState(null);
+
+    useEffect(() => {
+        GETRawMaterialOrder();
+    }, [])
+    
+
+    //                      Form
+
+    //Init valores form
+    itemMaterial.map(item => {
+        if (formMaterial.length < itemMaterial.length) {
+            formMaterial = [...formMaterial, {
+                raw_material_id: item.k_id, //Prop
+                // n_name: item.n_name, //Prop
+                q_quantity: 0,  //La cambia el  usuario
+                v_unit_price: 100 //Default
+            }];
+        }
+    });
+
+
+    const handleFormChange = (index, event) => {
+        let data = [...formMaterial];
+        data[index][event.target.name] = event.target.value;
+        setFormMaterial(data);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        eventBuyMaterialOrder();
+    }
+
+
+    //Proveedores
     const itemSupplier = [];
     supplier.map(item => {
         itemSupplier.push(item.n_name)
     })
 
-    const [formMaterial, setFormMaterial] = useState({});
 
-    useEffect(() => {
-        itemMaterial.map(campo => { //Campos dinamicos form
-            formMaterial[campo.n_name] = 0;
-        })
-    }, [])
-
-
-
-    const handleChange = (e) => {
-        setFormMaterial({
-            ...formMaterial,
-            [e.target.name]: e.target.value,
-        });
-        // console.log(e.target.name, e.target.value);
+    //                          POST raw_material_order
+    const POSTRawMaterialOrder = async (object) => {
+        const response = await fetch('http://localhost:8081/raw_material_order/save',
+            {
+                method: 'POST',
+                body: JSON.stringify(object),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        const data = await response.json();
+        console.log(data);
+    }
+    const GETRawMaterialOrder = async () => {
+        const response = await fetch(`http://localhost:8081/raw_material_order/findAll`);
+        const data = await response.json();
+        setID_MaterialOrder(data.length+1);
     }
 
-    const  handleSubmit = (e) => {
-        e.preventDefault();
-        const test = formMaterial;
-        console.log(test);
+    const eventBuyMaterialOrder = () => {
+        
+
+        let object = {
+            raw_material_order:{
+                k_id: ID_MaterialOrder,
+                supplier_id:12345, //Pendiente
+                admin_id:789456123, //Modulo Daniel
+                v_total_price:0.0 //Pendiente
+            },
+            raw_material_order_items:formMaterial
+        }
+
+        POSTRawMaterialOrder(object);
     }
 
 
@@ -45,17 +94,32 @@ export default function FormOrderRawMaterial({ itemMaterial, supplier }) {
                     itemMaterial.map((item, index) => {
                         return (
                             <div className='itemOrderRawMaterial' key={index}>
-                                <h3>{item.n_name}</h3>
-                                <div className='cantidadMaterial'>
-                                    <p>Cantidad:</p>
-                                    <input
-                                        type="number"
-                                        name={item.n_name}
-                                        min="0"
-                                        value={formMaterial[item.n_name]}
-                                        onChange={handleChange}
-                                    ></input>
-                                </div>
+                                <input
+                                    type="text"
+                                    name="raw_material_id"
+                                    value={item.k_id}
+                                    disabled={true}
+                                    onChange={event => { handleFormChange(index, event) }}>
+                                </input>
+                                <input
+                                    type="text"
+                                    name="n_name"
+                                    value={item.n_name}
+                                    disabled={true}>
+                                </input>
+                                <input
+                                    type="number"
+                                    name="q_quantity"
+                                    min={0}
+                                    value={formMaterial.q_quantity}
+                                    onChange={event => { handleFormChange(index, event) }}>
+                                </input>
+                                <input
+                                    type="text"
+                                    name="v_unit_price"
+                                    value={100}
+                                    disabled={true}>
+                                </input>
                             </div>
                         );
                     })
---
+++ src/componentes/formOrderRawMaterial/FormOrderRawMaterialTEST.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import './formOrderRawMaterial.css';
+import "react-widgets/styles.css";
+import Combobox from "react-widgets/Combobox";
+
+export default function FormOrderRawMaterial({ itemMaterial, supplier }) {
+
+    const itemSupplier = [];
+    supplier.map(item => {
+        itemSupplier.push(item.n_name)
+    })
+
+    const [formMaterial, setFormMaterial] = useState({});
+
+    useEffect(() => {
+        test();
+    }, [])
+
+    const test = () =>{
+        itemMaterial.map(campo => { //Campos dinamicos form
+            formMaterial[campo.n_name] = '';
+        })
+
+    }
+
+
+
+    const handleChange = (e) => {
+        setFormMaterial({
+            ...formMaterial,
+            [e.target.name]: e.target.value,
+        });
+        // console.log(e.target.name, e.target.value);
+    }
+
+    const  handleSubmit = (e) => {
+        e.preventDefault();
+        const test = formMaterial;
+        console.log(test);
+    }
+
+
+    return (
+        <div className='containerForm'>
+            <h1>Realizar pedido de materia prima</h1>
+            <hr></hr>
+
+            <form onSubmit={handleSubmit}>
+                {
+                    itemMaterial.map((item, index) => {
+                        return (
+                            <div className='itemOrderRawMaterial' key={index}>
+                                <h3>{item.n_name}</h3>
+                                <div className='cantidadMaterial'>
+                                    <p>Cantidad:</p>
+                                    <input
+                                        type="number"
+                                        name={item.n_name}
+                                        min="0"
+                                        value={formMaterial[item.n_name]}
+                                        onChange={handleChange}
+                                        // disabled={true}
+                                    ></input>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+
+                <div className='footerItemMaterial'>
+                    <div className='flex spaceBet'>
+                        <h3 className='proveedor'>Proveedor:</h3>
+                        <Combobox
+                            defaultValue=""
+                            data={itemSupplier}
+                        />
+                    </div>
+                    <button>Comprar</button>
+                </div>
+            </form>
+        </div>
+    )
+}
